1b6931fe274534af9e2fff39a20542ebbf386b32
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-08-20 09:58:04 -0500

    Instalacion de maven

---
+++ src/test/java/com/example/servidor_web_spring/ServidorWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servidor_web_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

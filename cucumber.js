513bc04668e98e8317c077f5148ffde9aa5d7d52
londgav01S <143038512+londgav01S@users.noreply.github.com>
2025-11-17 17:54:39 -0500

    feat: integrar Allure para Cucumber y publicar resultados en Jenkins

--- cucumber.js
+++ cucumber.js
@@ -1,14 +1,17 @@
-export default {
+module.exports = {
   default: {
     requireModule: ['ts-node/register'],
     require: ['features/support/**/*.ts'],
     format: [
       'progress-bar',
       'html:reports/cucumber-report.html',
-      'json:reports/cucumber-report.json'
+      'json:reports/cucumber-report.json',
+      '@shelex/cucumber-allure'
     ],
     formatOptions: {
-      snippetInterface: 'async-await'
-    }
+      snippetInterface: 'async-await',
+      resultsDir: 'allure-results'
+    },
+    publishQuiet: true
   }
 };
